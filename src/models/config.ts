6269dec97554d3c1a403a1c8719f6b8a5d55684c
Jackson Rosenthal <pwrxpsycho@gmail.com>
2020-12-22 20:12:50 +0000

    Update config.ts

--- src/models/config.ts
+++ src/models/config.ts
@@ -2,5 +2,5 @@ import * as env from 'dotenv'
 import { channelID } from '../config.json';
 
 export const token: string = process.env.DISCORD_TOKEN;
-export const nasaToken: string = process.env.DISCORD_TOKEN;
+export const nasaToken: string = process.env.API_KEY;
 export const chID: string = channelID;
