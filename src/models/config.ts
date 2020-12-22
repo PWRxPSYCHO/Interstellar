0c8bd9c5b57da5fe7af3d201e1f142da37cf863c
Jackson Rosenthal <pwrxpsycho@gmail.com>
2020-12-22 09:47:29 -0500

    updating for CI/CD

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,7 +1,6 @@
-import { apiToken } from '../config.json';
-import { nasaAPIKey } from '../config.json';
+import { env } from 'process';
 import { channelID } from '../config.json';
 
-export const token: string = apiToken;
-export const nasaToken: string = nasaAPIKey;
+export const token: string = env.DISCORD_TOKEN;
+export const nasaToken: string = env.API_KEY;
 export const chID: string = channelID;
